768e07b9e9571030a63f713bf54e221caab4a67c
akrem0-jr <abdulbasetadem@gmail.com>
2024-10-20 15:03:53 +0300

    some pages added

---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,52 @@
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options = {
+  providers: [
+    GitHubProvider({
+      profile(profile) {
+        console.log("GitHub profile", profile);
+        let userRole = "GitHub User";
+        if (profile?.email === "abdulbasetadem@gmail.com") {
+          userRole = "admin";
+        }
+
+        return {
+          ...profile,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+
+    GoogleProvider({
+      profile(profile) {
+        console.log("Google profile", profile);
+        let userRole = "Google User"; // Define userRole for Google
+        if (profile?.email === "abdulbasetadem@gmail.com") {
+          userRole = "admin";
+        }
+
+        return {
+          ...profile,
+          id: profile.sub, // Map Google's sub to id
+          role: userRole,
+        };
+      },
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+    },
+  },
+};
